bab25e3aa1b1dd1849cda151c7c550741f60a8f4
sarnowski <sarnowski@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-02-08 19:09:41 +0000

    IpcCommandExecutor interface added

---
+++ src/main/java/de/cosmocode/palava/ipc/IpcCommandExecutor.java
@@ -0,0 +1,41 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007-2010  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.cosmocode.palava.ipc;
+
+import java.util.Map;
+
+/**
+ * Provides the interface for a Command Executor
+ * implementation.
+ *
+ * @author Tobias Sarnowski
+ */
+public interface IpcCommandExecutor {
+
+	/**
+	 * Execute the given command and the given call, respecting the filter chain.
+	 *
+	 * @param ipcCommand the command to execute
+	 * @param ipcCall the call to execute the command with
+	 * @return the result of the command execution
+	 */
+	Map<String,Object> execute(IpcCommand ipcCommand, IpcCall ipcCall);
+
+}
