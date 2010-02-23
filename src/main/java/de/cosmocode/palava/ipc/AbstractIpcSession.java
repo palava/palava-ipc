72b4cc34fdfebe4acef90b1ae7dd9508411e2023
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-02-23 13:40:52 +0000

    fixed eclipse compiler bug with nested generic methods

--- src/main/java/de/cosmocode/palava/ipc/AbstractIpcSession.java
+++ src/main/java/de/cosmocode/palava/ipc/AbstractIpcSession.java
@@ -1,3 +1,22 @@
+/**
+ * palava - a java-php-bridge
+ * Copyright (C) 2007-2010  CosmoCode GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package de.cosmocode.palava.ipc;
 
 import java.util.Date;
@@ -65,7 +84,7 @@ public abstract class AbstractIpcSession extends AbstractIpcScopeContext impleme
     @Override
     public <K, V> V get(K key) {
         touch();
-        return super.get(key);
+        return super.<K, V>get(key);
     }
 
     @Override
@@ -83,7 +102,7 @@ public abstract class AbstractIpcSession extends AbstractIpcScopeContext impleme
     @Override
     public <K, V> V remove(K key) {
         touch();
-        return super.remove(key);
+        return super.<K, V>remove(key);
     }
 
     @Override
